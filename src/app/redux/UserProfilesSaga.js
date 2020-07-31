5171d6a0d3edccc8b38894ac58054ac5bf52c447
jsalyers <jsalyers@syncad.com>
2020-07-31 11:37:10 -0400

    [JES] WIP. Mostly finished condenser changes for decentralized system. Translations for other languages (or at least placeholders) should probably be added, and should come up with suggested accounts to follow for the newbies

--- src/app/redux/UserProfilesSaga.js
+++ src/app/redux/UserProfilesSaga.js
@@ -3,10 +3,24 @@ import * as userProfileActions from './UserProfilesReducer';
 import { callBridge } from 'app/utils/steemApi';
 
 const FETCH_PROFILE = 'userProfilesSaga/FETCH_PROFILE';
+const FETCH_LISTS = 'userProfilesSaga/FETCH_LISTS';
 
 export const userProfilesWatches = [
     takeLatest(FETCH_PROFILE, fetchUserProfile),
+    takeLatest(FETCH_LISTS, fetchLists),
 ];
+
+export function* fetchLists(action) {
+    const { observer, follow_type } = action.payload;
+    const ret = yield call(callBridge, 'get_follow_list', {
+        observer,
+        follow_type,
+    });
+    if (!ret) throw new Error('Account not found');
+    yield put(
+        userProfileActions.addList({ username: observer, listed_accounts: ret })
+    );
+}
 
 export function* fetchUserProfile(action) {
     const { account, observer } = action.payload;
@@ -21,6 +35,10 @@ export function* fetchUserProfile(action) {
 export const actions = {
     fetchProfile: payload => ({
         type: FETCH_PROFILE,
+        payload,
+    }),
+    fetchLists: payload => ({
+        type: FETCH_LISTS,
         payload,
     }),
 };
