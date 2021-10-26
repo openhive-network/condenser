c21e90deaf29ffb0d1a6576912f2ca753c826c65
NGUYEN DINH Quoc-Huy <quochuy@gmail.com>
2021-10-26 20:40:17 +1100

    Display RC level

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -3,3 +3,4 @@ export const VIEW_MODE_WHISTLE = 'whistle';
 export const WHISTLE_SIGNUP_COMPLETE = 'whistle_signup_complete';
 export const SIGNUP_URL = 'https://signup.hive.io';
 export const SUBMIT_FORM_ID = 'submitStory';
+export const EVENT_OPERATION_BROADCAST = 'event_operation_broadcast';
