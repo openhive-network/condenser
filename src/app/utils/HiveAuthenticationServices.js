825d20b06dbfd2a454d0aa7d6a2aa20930804893
NGUYEN DINH Quoc-Huy <quochuy@gmail.com>
2022-02-10 16:01:41 +1100

    Re-attache HAS if coming back from background

--- src/app/utils/HiveAuthenticationServices.js
+++ src/app/utils/HiveAuthenticationServices.js
@@ -118,6 +118,18 @@ const clearLoginInstructions = () => {
 };
 
 const login = async (username, callbackFn) => {
+    let visibilityChange;
+    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+        visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+        visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+        visibilityChange = "webkitvisibilitychange";
+    }
+    document.addEventListener(visibilityChange, () => {
+        HAS.default.connect();
+    }, false);
+
     updateLoginInstructions(tt('hiveauthservices.connecting'));
 
     setUsername(username);
