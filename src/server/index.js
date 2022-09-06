5c0ab86a8e3af19af071b814fee40e8abdb39911
Mahdi Yari <m.yary15@gmail.com>
2022-09-06 21:57:20 +0430

    Add the config for chain_id + fix initial uri set

--- src/server/index.js
+++ src/server/index.js
@@ -37,6 +37,7 @@ global.$STM_Config = {
     referral: config.get('referral'),
     rebranded_api: true,
     default_observer: config.get('default_observer'),
+    chain_id: config.get('chain_id')
 };
 
 const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
