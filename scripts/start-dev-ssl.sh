f7610d13b7f2c5b04d929609ce7a5d56bf5419cf
Wojciech Barcik <wbarcik@syncad.com>
2023-05-24 11:50:22 +0200

    (#212) Improve scripts/start-dev-ssl.sh script

--- scripts/start-dev-ssl.sh
+++ scripts/start-dev-ssl.sh
@@ -10,7 +10,8 @@ ROCKET_CHAT_URI="${1:-https://rocket-chat.local.wet.ovh}"
 export SDC_OPENHIVE_CHAT_API_URI="${ROCKET_CHAT_URI}" ;
 export SDC_OPENHIVE_CHAT_URI="${ROCKET_CHAT_URI}" ;
 export SDC_OAUTH_SERVER_ENABLE="yes" ;
-export SDC_OAUTH_SERVER_CLIENTS="`cat config/oauth-server-clients-development-ssl.json`" ;
+export SDC_OAUTH_SERVER_CLIENTS="`cat config/oauth-server-clients-development-ssl.json \
+        | sed \"s|https://rocket-chat.local.wet.ovh|${ROCKET_CHAT_URI}|\"`" ;
 
 export SDC_OPENHIVE_CHAT_IFRAME_INTEGRATION_ENABLE="yes" ;
 export SDC_OPENHIVE_CHAT_IFRAME_VISIBLE="yes" ;
